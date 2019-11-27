56977a17ec9e483a5147ac536544d180e402277c
KISHOR <KISHOR@LAPTOP-DPM6H1RM>
2019-11-27 17:06:58 +0530

    commit for angular

--- src/test/java/stepDefinition/Hooks.java
+++ src/test/java/stepDefinition/Hooks.java
@@ -2,6 +2,9 @@ package stepDefinition;
 
 import org.openqa.selenium.WebDriver;
 
+import com.paulhammant.ngwebdriver.NgWebDriver;
+
 public class Hooks {
 	static WebDriver driver;
+	static NgWebDriver ngDriver;
 }
